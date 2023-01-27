f27911373d4d51e8508641205e102e2022ce6553
LimeGradient <kfb13@scasd.org>
2023-01-27 00:02:13 -0500

    CLEAN: Unnessecary Struct

--- math/xmath.h
+++ math/xmath.h
@@ -1,13 +1,6 @@
 #ifndef XMATH
 #define XMATH
 
-struct Binomial
-{
-    int a; // First number
-    int b; // Second Number
-};
-typedef struct Binomial Binomial;
-
 int factor_binomial_to_gcf(int a, int b);
 int canDivide(int f, int l);
 int isInteger(double val);
